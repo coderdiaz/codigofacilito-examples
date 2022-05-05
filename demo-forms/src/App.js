aa2fbf8fd40bf9d14adfc32dfc9c5b02c2860b9f
Javier Diaz <coderdiaz@gmail.com>
2022-05-05 15:26:33 -0500

    feat: added support for show controlled components

--- demo-forms/src/App.js
+++ demo-forms/src/App.js
@@ -1,9 +1,13 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [name, setName] = useState('')
+  const [lastName, setLastName] = useState('')
+
   const onSubmit = (e) => {
     e.preventDefault()
-    console.log('on submit')
+    console.log('on submit', name, lastName)
   }
 
   const handleButtonClick = (e) => {
@@ -18,11 +22,11 @@ function App() {
       <form onSubmit={(e) => onSubmit(e)}>
         <div>
           <label>Nombre</label>
-          <input type="text" />
+          <input value={name} type="text" onChange={(e) => setName(e.target.value)} />
         </div>
         <div>
           <label>Apellidos</label>
-          <input type="text" />
+          <input value={lastName} type="text" onChange={(e) => setLastName(e.target.value)} />
         </div>
         <button type="button" onClick={onSubmit}>Enviar</button>
         <button onClick={(e) => handleButtonClick(e)}>Quiero tener suerte</button>
