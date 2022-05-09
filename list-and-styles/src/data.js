3cf48b3f5aedb30d9d609fabffc888ccb7db3ac2
Javier Diaz <coderdiaz@gmail.com>
2022-05-09 16:38:05 -0500

    feat: added sample data

---
+++ list-and-styles/src/data.js
@@ -0,0 +1,34 @@
+export const comments = [
+  {
+    id: 1,
+    name: 'John Doe',
+    bio: 'Software Engineer learning about #web3',
+    body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed auctor dictum sem quis dictum. In sollicitudin lectus nec nisi suscipit fringilla. Morbi at efficitur erat.',
+    replies: [
+      {
+        id: 11,
+        name: 'John Doe',
+        bio: 'Software Engineer learning about #web3',
+        body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed auctor dictum sem quis dictum. In sollicitudin lectus nec nisi suscipit fringilla. Morbi at efficitur erat.',
+      },
+      {
+        id: 12,
+        name: 'John Doe',
+        bio: 'Software Engineer learning about #web3',
+        body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed auctor dictum sem quis dictum. In sollicitudin lectus nec nisi suscipit fringilla. Morbi at efficitur erat.',
+      },
+      {
+        id: 13,
+        name: 'John Doe',
+        bio: 'Software Engineer learning about #web3',
+        body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed auctor dictum sem quis dictum. In sollicitudin lectus nec nisi suscipit fringilla. Morbi at efficitur erat.',
+      },
+      {
+        id: 14,
+        name: 'John Doe',
+        bio: 'Software Engineer learning about #web3',
+        body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed auctor dictum sem quis dictum. In sollicitudin lectus nec nisi suscipit fringilla. Morbi at efficitur erat.',
+      },
+    ]
+  }
+]
